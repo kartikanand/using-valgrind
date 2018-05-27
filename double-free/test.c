c7f504e179069b2af9aa7f26fe2a687e9fa063a4
kartik <kartikanand1992@gmail.com>
2018-05-27 22:25:10 +0530

    double free test-case

---
+++ double-free/test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char** argv) {
+    int* p = (int*)malloc(sizeof(int));
+    *p = 3;
+    printf("pointer value : %d\n", *p);
+
+    free(p);
+    free(p);
+
+    return 0;
+}
